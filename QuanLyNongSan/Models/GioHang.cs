c233df5789888d9adc20487f55dd49943216c6e3
Hiep Dang <76516081+dangvuhiep7500@users.noreply.github.com>
2021-07-22 21:12:57 +0700

    Initial commit

---
+++ QuanLyNongSan/Models/GioHang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QuanLyNongSan.Models;
+
+namespace QuanLyNongSan.Models
+{
+    public class GioHang
+    {
+        public NongSan nongsan { set; get; }
+        public int so_luong { set; get; }
+    }
+}
---
+++ QuanLyNongSan/Models/NongSanVN.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace QuanLyNongSan.Models
+{
+    public partial class NongSanVN : DbContext
+    {
+        public NongSanVN()
+            : base("name=NongSanVN")
+        {
+        }
+
+        public virtual DbSet<LoaiN> LoaiNS { get; set; }
+        public virtual DbSet<NguoiDung> NguoiDungs { get; set; }
+        public virtual DbSet<NongSan> NongSans { get; set; }
+        public virtual DbSet<TinTuc> TinTucs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LoaiN>()
+                .HasMany(e => e.NongSans)
+                .WithRequired(e => e.LoaiN)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<NguoiDung>()
+                .HasMany(e => e.TinTucs)
+                .WithRequired(e => e.NguoiDung)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
